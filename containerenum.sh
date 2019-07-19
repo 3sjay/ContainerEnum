a6b22c2384ec9eace3222e36b81a5278a03b9d00
3sjay <39624947+3sjay@users.noreply.github.com>
2019-07-19 09:00:09 +0200

    Create containerenum.sh

---
+++ containerenum.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+echo "[*] Checking dmesg for container info"
+dmesg 2>/dev/null | grep "docker\|lxc"
+
+echo
+echo
+
+if [ -x /.dockerenv ]; then
+	echo "[*] /.dockerenv found"
+fi
+
+ntimes=$(grep docker /proc/$$/cgroup | wc -l)
+if [ $ntimes -gt 0 ]; then
+	echo "[*] Found occurence of docker in /proc/<pid>/cgroup/ " $ntimes " times"
+fi
+
+
+ntimes=$(grep kube /proc/$$/cgroup | wc -l)
+if [ $ntimes -gt 0 ]; then
+	echo "[*] Found occurence of kube in /proc/<pid>/cgroup/\nHello Cluster :)" $ntimes " times"
+fi
+
+
+ntimes=$(grep docker /proc/$$/cpuset | wc -l)
+if [ $ntimes -gt 0 ]; then
+	echo "[*] Found occurence of docker in /proc/<pid>/cpuset/ " $ntimes " times"
+fi
+
+
+ntimes=$(grep kube /proc/$$/cpuset | wc -l)
+if [ $ntimes -gt 0 ]; then
+	echo "[*] Found occurence of kube in /proc/<pid>/cpuset/\nHello Cluster :) " $ntimes " times"
+fi
+
+
+ntimes=$(grep docker /proc/$$/mountinfo | wc -l)
+if [ $ntimes -gt 0 ]; then
+	echo "[*] Found occurence of docker in /proc/<pid>/mountinfo " $ntimes " times"
+fi
+
+
+ntimes=$(grep docker /proc/$$/mounts | wc -l)
+if [ $ntimes -gt 0 ]; then
+	echo "[*] Found occurence of docker in /proc/<pid>/mounts " $ntimes " times"
+fi
+
+
+if [ $(grep overlay /proc/$$/mountstats | wc -l) -gt 0 ]; then
+	if [ $(grep docker /proc/$$/mountstats | wc -l) -eq 0 ]; then
+		echo -ne "[*] overlay in /proc/<pid>/mounstats but not docker"
+		echo "		if docker would be in there we would be on the host"
+	fi
+fi
+
+
+if [ $(grep /etc/resolv.conf /proc/$$/mountstats|wc -l) -gt 0 ]; then
+	echo "[*] Found /etc/resolv.conf in /proc/<pid>/mountstats"
+fi
+
+if [ $(grep /etc/hostname /proc/$$/mountstats|wc -l) -gt 0 ]; then
+	echo "[*] Found /etc/hostname in /proc/<pid>/mountstats"
+fi
+
+if [ $(grep /etc/hosts /proc/$$/mountstats|wc -l) -gt 0 ]; then
+	echo "[*] Found /etc/hosts in /proc/<pid>/mountstats"
+fi
+
+
+seccomp=$(grep Seccomp /proc/$$/status | awk '{ print $2 }')
+if [ $seccomp -eq 0 ]; then
+	echo "[*] No Seccomp Profile applied."
+elif [ $seccomp -eq 1 ]; then
+	echo "[*] Seccomp set to SECCOMP_MODE_STRICT"
+elif [ $seccomp -eq 2 ]; then
+	echo "[*] Seccomp set to SECCOMP_MODE_FILTER"
+fi
+
+
+# Check for UID remapping
+first=$(cat /proc/$$/uid_map | awk '{ print $1 }')
+second=$(cat /proc/$$/uid_map | awk '{ print $2 }')
+if [ $first == $second ]; then
+	echo "[*] Within Host UID Namespace!"
+fi
+
+
+# TODO: Make checks accurate 
+if [ $(ip a 2>/dev/null | grep docker | wc -l ) -gt 0 ] || [ $(ifconfig 2>/dev/null | grep docker | wc -l ) -gt 0 ]; then
+	echo "[*] Within Host Network Namspace!"
+fi
+
+
+# full capability set
+capsetEff=$(grep CapEff /proc/$$/status |awk '{ print $2 }')
+capsetInh=$(grep CapInh /proc/$$/status |awk '{ print $2 }')
+
+if [ "0000003fffffffff" == ${capsetEff} ] && [ "0000003fffffffff" == ${capsetInh} ]; then
+	echo "[*] All Linux Capabilities available\n\tHere we go..."
+fi
+
+
+
+
+## To Check:
+# What is this file meant to do:
+#root@88bdee8239e5:/proc/1# cat projid_map
+#         0          0 4294967295
